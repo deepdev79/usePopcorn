987443081905fa3b28926fd98432e478a6c8b705
Deepankar <deepankar7493@gmail.com>
2025-12-14 12:43:52 +0530

    practice section TextE complete

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // import './index.css'
 // import App from './App.tsx'
-import StarRating from "./StarRating";
+// import StarRating from "./StarRating";
+import TextE from "./TextE";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <StarRating />
+    <TextE />
   </StrictMode>
 );
